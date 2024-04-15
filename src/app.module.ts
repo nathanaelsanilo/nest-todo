611f8462e76b62db363f1fa5eca4c789b96cd14c
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-04-15 10:54:02 +0700

    refactor: use config service, add global prefix, enable versioning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: 'localhost',
-        port: 5433,
-        username: '',
-        password: '',
-        database: '',
-        synchronize: true,
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: process.env.NODE_ENV !== 'production',
         autoLoadEntities: true,
       }),
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1'],
+  });
+  app.setGlobalPrefix('/api');
+  const config = app.get(ConfigService);
+
+  await app.listen(config.get('APP_PORT'));
 }
 bootstrap();
